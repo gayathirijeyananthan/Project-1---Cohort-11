893bd37f0918df3c34dd6dd74e23c5cfb467f251
gayathirijeyananthan <gayathirijeyananthan@gmail.com>
2025-06-05 11:32:21 +0530

    first commit

---
+++ backend/controller/authController.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ name, email, password, role });
+    const token = generateToken(user);
+    res.status(201).json({ token, message:"User registered successfully"});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const token = generateToken(user);
+    res.json({ token, user, message:"You successfully loged in" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controller/authController');
+const { authMiddleware } = require('../middleware/authMiddleware');
+const { roleMiddleware } = require('../middleware/roleMiddleware');
+
+router.post('/register', register);
+router.post('/login', login);
+
+// Example protected routes
+router.get('/admin', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  res.send('Welcome Admin');
+});
+
+router.get('/profile', authMiddleware, roleMiddleware('tailor', 'mechanic', 'partner'), (req, res) => {
+  res.send(`Welcome ${req.user.role}`);
+});
+
+module.exports = router;
