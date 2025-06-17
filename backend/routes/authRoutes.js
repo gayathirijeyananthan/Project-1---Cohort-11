0e857a6e3926df91efaf442ddfd00de7fb264e71
gayathirijeyananthan <gayathirijeyananthan@gmail.com>
2025-06-17 09:31:20 +0530

    updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,7 +12,7 @@ router.get('/admin', authMiddleware, roleMiddleware('admin'), (req, res) => {
   res.send('Welcome Admin');
 });
 
-router.get('/profile', authMiddleware, roleMiddleware('tailor', 'mechanic', 'partner'), (req, res) => {
+router.get('/profile', authMiddleware, roleMiddleware('User'), (req, res) => {
   res.send(`Welcome ${req.user.role}`);
 });
 
